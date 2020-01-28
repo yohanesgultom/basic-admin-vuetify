18070c8f5ab6b831bd13a91240521e6eb5cd8cfd
Yohanes Gultom <yohanes.gultom@gmail.com>
2020-01-28 20:33:24 +0700

    add crud data table

--- js/7228.25e0b31d.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["7228"],{7228:function(e,t,r){"use strict";r.r(t);var n=function(){var e=this,t=e.$createElement,r=e._self._c||t;return r("v-content",[r("v-app",[r("core-filter"),r("core-toolbar"),r("core-drawer"),r("v-fade-transition",{attrs:{mode:"out-in"}},[r("router-view")],1)],1)],1)},o=[],a=r("2877"),c={},i=Object(a["a"])(c,n,o,!1,null,null,null);t["default"]=i.exports}}]);
-//# sourceMappingURL=7228.25e0b31d.js.map
